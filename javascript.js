59dc8fc7616cb61adb113dfa8ae5e7c024261e81
satish-knwr <satish.knwr@gmail.com>
2015-04-01 15:24:51 +0545

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function show(){
+	var a=document.getElementById("Name").value;
+	var b=document.getElementById("Email").value;
+	var c=document.getElementById("Phone").value;
+	var d = document.getElementById("d").innerHTML=a;
+	document.getElementById("e").innerHTML=b;
+	document.getElementById("f").innerHTML=c;
+	}
